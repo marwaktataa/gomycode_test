064e1b8ffe3cc4d1ecfbf47e796f8e6db300527e
marwaktataa <marwaktata15@gmail.com>
2019-11-19 15:09:44 +0100

    Updates

---
+++ precache-manifest.ac18f055736c15b2cb6f363f5921d77c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4408627a2f8203e28db1661c0db1dd",
+    "url": "/gomycode_task/index.html"
+  },
+  {
+    "revision": "7f910e33b3b0653af503",
+    "url": "/gomycode_task/static/css/main.5c0d9cce.chunk.css"
+  },
+  {
+    "revision": "8b05a2b7c644c543a7cb",
+    "url": "/gomycode_task/static/js/2.ce9847a6.chunk.js"
+  },
+  {
+    "revision": "7f910e33b3b0653af503",
+    "url": "/gomycode_task/static/js/main.6234160d.chunk.js"
+  },
+  {
+    "revision": "e95228d5451197544f6e",
+    "url": "/gomycode_task/static/js/runtime-main.d9e7b290.js"
+  }
+]);
